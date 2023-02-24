a5f1660d7235bab34f029207ea117ed1d88c8216
Augustine Menezes <amenezes927@gmail.com>
2023-02-24 20:34:08 +0530

    Changes

--- src/Components/RatingComponent.js
+++ src/Components/RatingComponent.js
@@ -35,13 +35,13 @@ export const RatingComponent = () => {
           <div className="ratings__reviews-container-left-wrapper">
             <div class="ratings-and-reviews__bottom-left">
               <div class="ratings-and-reviews__author">
-                <p>BHAVIN</p>
-                <span>
+                <div>BHAVIN</div>
+                <div>
                   <img
                     src="https://ik.ourlittlejoys.com/mosaic-wellness/image/upload/v1629700121/staging/Home/Images/Group_2298.png"
                     alt="verified mark"
                   />
-                </span>
+                </div>
               </div>
               <div class="ratings-and-reviews__date">02/01/2023</div>
             </div>
@@ -55,13 +55,13 @@ export const RatingComponent = () => {
           <div className="ratings__reviews-container-left-wrapper">
             <div class="ratings-and-reviews__bottom-left">
               <div class="ratings-and-reviews__author">
-                <p>BHAVIN</p>
-                <span>
+                <div>BHAVIN</div>
+                <div>
                   <img
                     src="https://ik.ourlittlejoys.com/mosaic-wellness/image/upload/v1629700121/staging/Home/Images/Group_2298.png"
                     alt="verified mark"
                   />
-                </span>
+                </div>
               </div>
               <div class="ratings-and-reviews__date">02/01/2023</div>
             </div>
